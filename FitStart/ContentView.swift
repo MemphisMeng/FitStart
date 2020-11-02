d23405bbfe7e1adf0b518bd2d253f73b633037c1
Minglan Zheng <59420248+mzheng27@users.noreply.github.com>
2020-11-02 00:15:14 -0500

    Homepage layout

--- FitStart/ContentView.swift
+++ FitStart/ContentView.swift
@@ -6,32 +6,138 @@
 //
 import SwiftUI
 
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
 struct Task: Identifiable {
     var id: Int
     let message, imageName: String
 }
 
+var tabs = ["home", "meal", "fitness", "pal"]
+
+struct TabButton : View {
+    var image : String
+    @Binding var selectedTab : String
+    var body : some View {
+        Button(action: {selectedTab == image}) {
+            Image(image)
+                .resizable()
+                .renderingMode(.template)
+                .foregroundColor(selectedTab == image ? Color("tab") : Color.black.opacity(0.4))
+                .frame(width: 40, height: 40)
+                .padding()
+        }
+        
+        
+    }
+        
+    
+}
+
 struct ContentView: View {
-    let tasks: [Task] = [
-        .init(id: 0, message: "Exercise for 20 minutes, following the cardio and arm animations", imageName: "Running"),
-        .init(id: 1, message: "Drink 3 cups of water", imageName: "Drink_Water"),
-        .init(id: 2, message: "Go to bed before 10pm", imageName: "Exercise")
-    ]
+    @State var selectedTab = ""
     
     var body: some View {
-        NavigationView {
-            List {
-                QuoteView()
-                Text("Goals").font(.largeTitle)
-                ForEach(tasks) { task in
-                    TaskRow(task: task)
+        ZStack(alignment: Alignment(horizontal: .center, vertical: .bottom)) {
+            Home()
+            HStack(spacing: 0) {
+                ForEach(tabs, id: \.self) {image in
+                    TabButton(image: image, selectedTab: $selectedTab)
+                    if image != tabs.last {
+                        Spacer(minLength: 2)
+                    }
+                    
                 }
-                //
             }
+            .padding(.horizontal, 20)
+            .padding(.vertical, 12)
+            .background(Color.white)
+            .clipShape(Capsule())
+            .padding(.horizontal)
             
         }
     }
+    
 }
+
+struct Home: View {
+    @State var txt = ""
+    let tasks: [Task] = [
+            .init(id: 0, message: "Exercise for 20 minutes, following the cardio and arm animations", imageName: "Running"),
+            .init(id: 1, message: "Drink 3 cups of water", imageName: "Drink_Water"),
+            .init(id: 2, message: "Go to bed before 10 pm", imageName: "Exercise")
+        ]
+    var body: some View {
+        VStack{
+            HStack{
+                VStack(alignment: .leading, spacing: 15) {
+                    Text("Hello James").font(.title).fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    
+                    Text("The secret of getting ahead is getting started.” – Mark Twain")
+                }.foregroundColor(.black)
+                Spacer(minLength: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/)
+                VStack {
+                    Button(action: {}) {
+                        Image("addPeople")
+                            .resizable()
+                            .renderingMode(.original)
+                            .overlay(Rectangle().stroke(Color.blue, lineWidth: 3))
+                            .frame(width: 37, height: 37)
+                    }.padding()
+                    Button(action: {}) {
+                        Image("profile")
+                            .resizable()
+                            .renderingMode(.original)
+                            .overlay(Circle().stroke(Color.blue, lineWidth: 3))
+                            .frame(width: 45, height: 45)
+                    }
+                }
+            }.padding()
+            ScrollView(/*@START_MENU_TOKEN@*/.vertical/*@END_MENU_TOKEN@*/, showsIndicators: false) {
+                VStack {
+                    
+                    
+                    HStack{
+                        Text("Daily Goals")
+                            .font(.title2)
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        Spacer(minLength: 0)
+                        Button(action: {}) {
+                            Text("View All")
+                        }
+                    }
+                    .foregroundColor(.black)
+                    .padding(.top, 25)
+                    
+                    Spacer(minLength: 15)
+                    
+                    HStack(spacing: 15) {
+                        TextField("Add a goal...", text: $txt)
+                    }
+                    .padding(.vertical, 12)
+                    .padding(.horizontal)
+                    .background(Color.white)
+                    .clipShape(Rectangle())
+                    
+                    Spacer(minLength: 8)
+                    
+                    LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 20), count: 1), spacing: 20) {
+                        ForEach (tasks) { task in
+                            TaskRow(task: task)
+                        }
+                    }
+                        
+                }.padding()
+            }
+//
+        }.background(Color.blue.opacity(0.07).ignoresSafeArea(.all, edges: .all))
+    }
+}
+
 
 struct TaskRow: View {
     let task: Task
@@ -49,26 +155,5 @@ struct TaskRow: View {
     }
 }
 
-struct QuoteView: View {
-    var body: some View {
-        ZStack {
-            Text("“The secret of getting ahead is getting started.” – Mark Twain.").bold().position(CGPoint(x: 150, y: 0))
-        }
-        
-    }
-}
 
-struct MenuBar: View {
-    var body: some View {
-        HStack {
-            
-        }
-    }
-    
-}
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}

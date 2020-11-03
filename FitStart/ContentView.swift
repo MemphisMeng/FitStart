c44763d65ca42d622f4fdec97dce6b5f8c5804fd
Minglan Zheng <59420248+mzheng27@users.noreply.github.com>
2020-11-02 19:10:19 -0500

    MenuBar: Icon name and pop up
    Show icon name and pop the icon upon selection

--- FitStart/ContentView.swift
+++ FitStart/ContentView.swift
@@ -19,18 +19,39 @@ struct Task: Identifiable {
 
 var tabs = ["home", "meal", "fitness", "pal"]
 
+
+//Button for the Bottom Menu Bar
 struct TabButton : View {
     var image : String
+    @Binding var centerX : CGFloat
+    var rect : CGRect
     @Binding var selectedTab : String
+    
     var body : some View {
-        Button(action: {selectedTab = image}) {
-            Image(image)
-                .resizable()
-                .renderingMode(.template)
-                .foregroundColor(selectedTab == image ? Color.blue.opacity(0.5) : Color.black.opacity(0.5))
-                .frame(width: 40, height: 40)
-                .padding()
-        }
+        
+        Button(action: {withAnimation(.spring()){
+            selectedTab = image
+            centerX = rect.midX
+            }
+        }, label: {
+            VStack {
+                Image(image)
+                    .resizable()
+                    .renderingMode(.template)
+                    .frame(width: 40, height: 40)
+                    .foregroundColor(selectedTab == image ? Color.blue.opacity(0.5) : Color.black.opacity(0.4))
+                    
+                Text(image)
+                    .font(.caption)
+                    .foregroundColor(.black)
+                    .opacity(selectedTab == image ? 1 : 0)
+            }
+            .padding(.top)
+            .frame(width: 70, height: 50)
+            //pop the icon selected
+            .offset(y: selectedTab == image ? -15 : 0)
+            
+        })
     
     }
         
@@ -40,45 +61,58 @@ struct TabButton : View {
 struct ContentView: View {
     @State var selectedTab = "home"
     
-    var body: some View {
-        ZStack(alignment: Alignment(horizontal: .center, vertical: .bottom)) {
-            
-            //swich tabs
-            TabView(selection: $selectedTab) {
-                Home()
-                    .tag("home")
-                Meal()
-                    .tag("meal")
-                Fitness()
-                    .tag("fitness")
-                Pal()
-                    .tag("pal")
-            }
-            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
-            .ignoresSafeArea(.all, edges: .bottom)
-            
-        
-            HStack(spacing: 0) {
-                ForEach(tabs, id: \.self) {image in
-                    TabButton(image: image, selectedTab: $selectedTab)
-                    if image != tabs.last {
-                        Spacer(minLength: 2)
-                    }
-                    
-                }
-            }
-            .padding(.horizontal, 20)
-            .padding(.vertical, 4)
-            .background(Color.white)
-            .clipShape(Capsule())
-            .shadow(color: Color.blue.opacity(0.15), radius: 5, x: 5, y: 5)
-            .shadow(color: Color.blue.opacity(0.15), radius: 5, x: -5, y: -5)
-            .padding(.horizontal)
-        }
-        .ignoresSafeArea(.keyboard, edges: .bottom)
-        .background(Color.blue.opacity(0.07).ignoresSafeArea(.all, edges: .all))
+    init() {
+        UITabBar.appearance().isHidden = true
     }
     
+    @State var centerX : CGFloat = 0
+//    @Environement(.verticalSizeClass) var size
+    var body: some View {
+            //swich tabs
+            VStack(spacing: 0) {
+                TabView(selection: $selectedTab) {
+                    Home()
+                        .tag("home")
+                    Meal()
+                        .tag("meal")
+                    Fitness()
+                        .tag("fitness")
+                    Pal()
+                        .tag("pal")
+                    
+                }
+            
+            
+            //for menu bar
+            HStack(spacing: 0) {
+                ForEach(tabs, id: \.self) {image in
+                    GeometryReader {reader in
+                        TabButton(image: image, centerX: $centerX, rect:reader.frame(in:.global), selectedTab: $selectedTab)
+                        //setting initial curve
+                            .onAppear(perform: {
+                                if image == tabs.first{
+                                    centerX = reader.frame(in: .global).midX
+                                }
+                            })
+                            
+                    }
+                    .frame(width: 70, height: 50)
+                    if image != tabs.last {Spacer(minLength: 0)}
+                }
+            }
+            .padding(.horizontal, 25)
+            .padding(.top)
+                //For Smaller size iphone padding will be 15
+            .padding(.bottom, UIApplication.shared.windows.first?.safeAreaInsets.bottom == 0 ? 15:
+                            UIApplication.shared.windows.first?.safeAreaInsets.bottom)
+            .background(Color.white.clipShape(AnimatedShape(centerX: centerX)))
+            .shadow(color: Color.blue.opacity(0.1), radius: 5, x: 0, y: -5)
+            .padding(.top, -15)
+            .ignoresSafeArea(.all, edges: .horizontal)
+        }
+        .background(Color.blue.opacity(0.07).ignoresSafeArea(.all, edges: .bottom))
+    }
+        
 }
 
 struct Home: View {
@@ -115,6 +149,7 @@ struct Home: View {
                     }
                 }
             }.padding()
+            //for goals
             ScrollView(/*@START_MENU_TOKEN@*/.vertical/*@END_MENU_TOKEN@*/, showsIndicators: false) {
                 VStack {
                     HStack{
@@ -157,7 +192,7 @@ struct Home: View {
     }
 }
 
-
+//The task list
 struct TaskRow: View {
     let task: Task
     var body: some View {
@@ -172,6 +207,24 @@ struct TaskRow: View {
             }.padding(.leading, 8)
         }.padding(.init(top: 12, leading: 0, bottom: 12, trailing: 0))
     }
+}
+
+//The Custom shape for poping up the Icon in the Menu Bar upon selection
+struct AnimatedShape: Shape {
+    var centerX : CGFloat
+    func path(in rect: CGRect) -> Path {
+        return Path{path in
+            path.move(to: CGPoint(x:0, y:15))
+            path.addLine(to: CGPoint(x:0, y:rect.height))
+            path.addLine(to: CGPoint(x:rect.width, y:rect.height))
+            path.addLine(to: CGPoint(x:rect.width, y:15))
+            
+            //curve
+            path.move(to: CGPoint(x: centerX - 35, y:15))
+            path.addQuadCurve(to: CGPoint(x: centerX + 35, y:15), control: CGPoint(x:centerX, y:-30))
+        }
+    }
+    
 }
 
 struct Meal: View {

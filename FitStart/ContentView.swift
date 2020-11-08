f9973ba6404f6d70035cafec96683345a71a0bca
Minglan Zheng <59420248+mzheng27@users.noreply.github.com>
2020-11-08 11:29:49 -0500

    LandingScreen

--- FitStart/ContentView.swift
+++ FitStart/ContentView.swift
@@ -7,6 +7,8 @@
 import SwiftUI
 
 struct ContentView_Previews: PreviewProvider {
+    
+    
     static var previews: some View {
         ContentView()
     }
@@ -66,55 +68,156 @@ struct ContentView: View {
     }
     
     @State var centerX : CGFloat = 0
+    @State var goToHome = false
 //    @Environement(.verticalSizeClass) var size
     var body: some View {
-            //swich tabs
-            VStack(spacing: 0) {
-                TabView(selection: $selectedTab) {
-                    Home()
-                        .tag("home")
-                    Meal()
-                        .tag("meal")
-                    Fitness()
-                        .tag("fitness")
-                    Pal()
-                        .tag("pal")
-                    
-                }
-            
-            
-            //for menu bar
-            HStack(spacing: 0) {
-                ForEach(tabs, id: \.self) {image in
-                    GeometryReader {reader in
-                        TabButton(image: image, centerX: $centerX, rect:reader.frame(in:.global), selectedTab: $selectedTab)
-                        //setting initial curve
-                            .onAppear(perform: {
-                                if image == tabs.first{
-                                    centerX = reader.frame(in: .global).midX
-                                }
-                            })
-                            
+        ZStack {
+            if goToHome {
+                //swich tabs
+                VStack(spacing: 0) {
+                    TabView(selection: $selectedTab) {
+                        Home()
+                            .tag("home")
+                        Meal()
+                            .tag("meal")
+                        Fitness()
+                            .tag("fitness")
+                        Pal()
+                            .tag("pal")
+                        
                     }
-                    .frame(width: 70, height: 50)
-                    if image != tabs.last {Spacer(minLength: 0)}
+                
+                
+                //for menu bar
+                HStack(spacing: 0) {
+                    ForEach(tabs, id: \.self) {image in
+                        GeometryReader {reader in
+                            TabButton(image: image, centerX: $centerX, rect:reader.frame(in:.global), selectedTab: $selectedTab)
+                            //setting initial curve
+                                .onAppear(perform: {
+                                    if image == tabs.first{
+                                        centerX = reader.frame(in: .global).midX
+                                    }
+                                })
+                                
+                        }
+                        .frame(width: 70, height: 50)
+                        if image != tabs.last {Spacer(minLength: 0)}
+                    }
                 }
+                .padding(.horizontal, 25)
+                .padding(.top)
+                    //For Smaller size iphone padding will be 15
+                .padding(.bottom, UIApplication.shared.windows.first?.safeAreaInsets.bottom == 0 ? 15:
+                                UIApplication.shared.windows.first?.safeAreaInsets.bottom)
+                .background(Color.white.clipShape(AnimatedShape(centerX: centerX)))
+                .shadow(color: Color.blue.opacity(0.1), radius: 5, x: 0, y: -5)
+                .padding(.top, -15)
+                .ignoresSafeArea(.all, edges: .horizontal)
             }
-            .padding(.horizontal, 25)
-            .padding(.top)
-                //For Smaller size iphone padding will be 15
-            .padding(.bottom, UIApplication.shared.windows.first?.safeAreaInsets.bottom == 0 ? 15:
-                            UIApplication.shared.windows.first?.safeAreaInsets.bottom)
-            .background(Color.white.clipShape(AnimatedShape(centerX: centerX)))
-            .shadow(color: Color.blue.opacity(0.1), radius: 5, x: 0, y: -5)
-            .padding(.top, -15)
-            .ignoresSafeArea(.all, edges: .horizontal)
+            .background(Color.blue.opacity(0.07).ignoresSafeArea(.all, edges: .bottom))
+            
+            } //end if
+            else {
+                OnBoardScreen()
+            }
         }
-        .background(Color.blue.opacity(0.07).ignoresSafeArea(.all, edges: .bottom))
+        .onReceive(NotificationCenter.default.publisher(for:Notification.Name("Success")), perform: {
+            _ in withAnimation{self.goToHome = true}
+        })
+            
     }
         
 }
 
+
+struct OnBoardScreen: View {
+    @State var maxWidth = UIScreen.main.bounds.width - 100
+    @State var offset : CGFloat = 0
+    var body: some View {
+        ZStack {
+            Color("Color")
+                .ignoresSafeArea(.all, edges: .all)
+            VStack {
+                Spacer(minLength: 0)
+                Text("Motivational Quote")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .foregroundColor(.white)
+                Text("A positive attitude gives you power over your circumstances instead of your circumstances having power over you.")
+                    .foregroundColor(.white)
+                    .padding()
+                    .padding(.bottom)
+                //                Image("Quote")
+                Spacer(minLength: 0)
+                ZStack {
+                    Capsule()
+                        .fill(Color.white.opacity(0.1))
+                    Text("SWIPE TO START")
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                        .padding(.leading, 30)
+                    //background progress
+                    HStack {
+                        Capsule()
+                            .fill(Color("red"))
+                            .frame(width: calculateWidth() + 65)
+                        Spacer(minLength: 0)
+                    }
+                    
+                    HStack {
+                        ZStack {
+                            Image(systemName: "chevron.right")
+                            Image(systemName: "chevron.right")
+                                .offset(x: -10)
+                        }
+                        .foregroundColor(.white)
+                        .offset(x:10)
+                        .frame(width: 65, height: 65)
+                        .background(Color("red"))
+                        .clipShape(Circle())
+                        .offset(x: offset)
+                        .gesture(DragGesture().onChanged(onChange(value:))
+                                    .onEnded(onEnd(value:)))
+                        
+                        
+                        Spacer()
+                    }
+                }
+                .frame(width: maxWidth, height: 65)
+                .padding(.bottom)
+            }
+        }
+    }
+    func calculateWidth() -> CGFloat {
+        let percent = offset/maxWidth
+        return percent * maxWidth
+    }
+    
+    
+    func onChange(value: DragGesture.Value) {
+        if value.translation.width > 0 && offset <= maxWidth - 65 {
+            offset = value.translation.width
+        }
+    }
+    
+    func onEnd(value: DragGesture.Value) {
+        withAnimation(Animation.easeOut(duration: 0.3)) {
+            if offset > 180 {
+                offset = maxWidth - 65
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.35) {
+                    NotificationCenter.default.post(name: NSNotification.Name("Success"), object: nil)
+                }
+            } else {
+                offset = 0
+            }
+        }
+    }
+}
+
+
+//The first frames
 struct Home: View {
     @State var txt = ""
     @State var edge = UIApplication.shared.windows.first?.safeAreaInsets
@@ -128,24 +231,22 @@ struct Home: View {
             HStack{
                 VStack(alignment: .leading, spacing: 15) {
                     Text("Hello James").font(.title).fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                    
-                    Text("The secret of getting ahead is getting started.” – Mark Twain")
                 }.foregroundColor(.black)
                 Spacer(minLength: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/)
                 VStack {
-                    Button(action: {}) {
-                        Image("addPeople")
-                            .resizable()
-                            .renderingMode(.original)
-                            .overlay(Rectangle().stroke(Color.blue, lineWidth: 3))
-                            .frame(width: 37, height: 37)
-                    }.padding()
+//                    Button(action: {}) {
+//                        Image("addPeople")
+//                            .resizable()
+//                            .renderingMode(.original)
+//                            .overlay(Rectangle().stroke(Color.blue, lineWidth: 3))
+//                            .frame(width: 37, height: 37)
+//                    }.padding()
                     Button(action: {}) {
                         Image("profile")
                             .resizable()
                             .renderingMode(.original)
-                            .overlay(Circle().stroke(Color.blue, lineWidth: 3))
-                            .frame(width: 45, height: 45)
+//                            .overlay(Circle().stroke(Color.blue, lineWidth: 3))
+                            .frame(width: 50, height: 45)
                     }
                 }
             }.padding()
@@ -237,11 +338,52 @@ struct Meal: View {
 
 struct Fitness: View {
     var body: some View {
-        VStack {
-            Text("Fitness")
+        VStack(spacing: 15) {
+            HStack {
+                VStack(alignment: .leading, spacing: 15) {
+                    Text("Fitness").font(.largeTitle)
+                }.foregroundColor(.blue)
+            }
+            Spacer()
+            SearchView().padding(.vertical, 15)
+//            ScrollView(.vertical, showsIndicators: false) {
+//                VStack(spacing: 15) {
+//                    ForEach(ex_datas) { i in
+//                        ScrollView(.horizontal, showsIndicators: false) {
+//                            HStack(spacing: 15) {
+//                                ForEach(i.row) {j in
+//                                    VStack(alignment: .leading, spacing: 12) {
+//                                        Image(j.image)
+//                                            .resizable()
+//                                            .renderingMode(.original)
+//                                            .frame(width: 50, height: 70)
+//                                        Text(j.name).font(.caption)
+//                                    }.foregroundColor(Color.black.opacity(0.5))
+//                                    .frame(width: 120)
+//                                }
+//                            }
+//                        }
+//                    }
+//                }
+//            }
         }
     }
 }
+
+struct SearchView : View {
+    @State var txt = ""
+    var body : some View {
+        HStack(spacing : 15) {
+            Image(systemName: "magnifyingglass").font(.body)
+            TextField("Search Exercise", text: $txt)
+        }.padding()
+        .foregroundColor(.black)
+        .cornerRadius(25)
+    }
+}
+
+
+
 
 struct Pal: View {
     var body: some View {
@@ -250,6 +392,8 @@ struct Pal: View {
         }
     }
 }
+
+
 
 
 

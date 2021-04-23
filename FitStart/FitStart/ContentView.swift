8a7708f2e1dde6c98f2b0a8bf7669ea5c7066427
MemphisMeng <1310750076@qq.com>
2021-04-23 19:18:17 -0400

    score update according to db

--- FitStart/FitStart/ContentView.swift
+++ FitStart/FitStart/ContentView.swift
@@ -164,6 +164,10 @@ struct Home: View {
                     .padding(.trailing)
             }
             .padding(.top)
+//            .onAppear() {
+//                print("view drawn")
+//                self.users.fetchData()
+//                }
 //            Spacer()
             LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 20), count: 2), spacing: 10) {
                 ForEach(mainfs) { f in
--- FitStart/FitStart/ViewModel/UserViewModel.swift
+++ FitStart/FitStart/ViewModel/UserViewModel.swift
@@ -10,32 +10,22 @@ import Firebase
 import FirebaseFirestore
 
 class UserViewModel: ObservableObject {
-    // TODO
-    let current_user_id = Auth.auth().currentUser!.uid
-    private var db = Firestore.firestore()
-    @Published var xp:Int?
-    
-    func fetchData(completion: @escaping () -> Void) {
-        let docRef = db.collection("users").document("7lqIqxc7SGPrbRhhQWZ0rdNuKnb2")
-//        docRef.getDocument { (document, error) in
-//            if let document = document, document.exists {
-//                let xp = document.data()!["xp"] as! Int
-//                // fetch the experience score
-//                completion()
-//            } else {
-//                print("Document does not exist")
-//            }
-//        }
+    @Published var xp: Int?
         
-        docRef.getDocument { snapshot, error in
-              print(error ?? "No error.")
-              self.xp = 0
-              guard let snapshot = snapshot else {
-                  completion()
-                  return
-              }
-            self.xp = (snapshot.data()!["xp"] as! Int)
-            completion()
+    init() {
+        // TODO
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        let docRef = Firestore.firestore().collection("users").document("7lqIqxc7SGPrbRhhQWZ0rdNuKnb2")
+
+        docRef.getDocument { (snapshot, error) in
+            if let doc = snapshot,
+               let xp = doc.get("xp") as? Int {
+                self.xp = xp
+            } else if let error = error {
+                print(error)
+            }
         }
     }
 }
